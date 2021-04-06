33092ac554ca46335f94e114cdfb0bafabc69b8c
EmanuelGF <emanuelfaisca@gmail.com>
2021-04-06 17:39:23 +0100

    importing react-bootstrap css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//importing css for react-bootstrap components
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
 
 ReactDOM.render(
   <React.StrictMode>
