40e0a445aa01146d3858faa438aa8a98cf6b7c66
inezabonte <inezabonte@gmail.com>
2021-05-08 22:14:18 +0200

    remove tests

--- cypress/integration/essential/home.spec.js
+++ cypress/integration/essential/home.spec.js
@@ -7,15 +7,6 @@ describe("Renders the homepage", () => {
 	it("Renders about page when the /about link is clicked", () => {
 		cy.visit("/");
 		cy.get(".nav-link").eq(1).click();
-
 		cy.get("h1").should("contain", "Welcome to the about page!");
-	});
-
-	it("Renders the footer correctly", () => {
-		cy.visit("/");
-		cy.get("footer").should(
-			"contain",
-			`©yourBizz ${new Date().toLocaleDateString()}`
-		);
 	});
 });
