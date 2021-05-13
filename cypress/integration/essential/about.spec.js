e99a47cfb74bc97c5a35330b98520b4c10e18977
EmanuelGF <emanuelfaisca@gmail.com>
2021-05-13 16:24:29 +0100

    Run cypress tests on multiple viewports. closes #25

--- cypress/integration/essential/about.spec.js
+++ cypress/integration/essential/about.spec.js
@@ -1,12 +1,25 @@
-describe("Renders the about page", () => {
-	it("Renders correctly", () => {
-		cy.visit("/about");
-		cy.get("h1").should("contain", "Welcome to the about page!");
-	});
+//Env variables on cypress.json
+const orientations = Cypress.env('orientations')
+const viewports = Cypress.env('viewports')
 
-	it("Renders home page when home link is clicked", () => {
-		cy.visit("/about");
-		cy.get("a").click();
-		cy.get("h1").should("contain", "React template");
+describe("Renders the about page", () => {
+	viewports.forEach(viewport => {
+		for (let i = 0; i < orientations.length; i++) {
+
+			it(`(${viewport}-${orientations[i]}) - Renders correctly.`, () => {
+				cy.viewport(viewport, orientations[i])
+				cy.visit("/about");
+				cy.get("h1").should("contain", "Welcome to the about page!");
+			});
+		
+			it(`(${viewport}-${orientations[i]}) - Renders home page when home link is clicked.`, () => {
+				cy.viewport(viewport, orientations[i])
+				cy.visit("/about");
+				cy.get("a").click();
+				cy.get("h1").should("contain", "React template");
+			});
+
+			if (viewport === "macbook-11") break; //Ensures tests only run once on this viewport. 
+		}
 	});
 });
--- cypress/integration/essential/home.spec.js
+++ cypress/integration/essential/home.spec.js
@@ -1,12 +1,25 @@
-describe("Renders the homepage", () => {
-	it("Renders correctly", () => {
-		cy.visit("/");
-		cy.get("h1").should("contain", "React template");
-	});
+//Env variables on cypress.json
+const orientations = Cypress.env('orientations')
+const viewports = Cypress.env('viewports')
 
-	it("Renders about page when the /about link is clicked", () => {
-		cy.visit("/");
-		cy.get(".nav-link").eq(1).click();
-		cy.get("h1").should("contain", "Welcome to the about page!");
+describe("Renders the homepage", () => {
+	viewports.forEach(viewport => {
+		for (let i = 0; i < orientations.length; i++) {
+
+			it(`(${viewport}-${orientations[i]}) - Renders correctly`, () => {
+				cy.viewport(viewport, orientations[i])
+				cy.visit("/");
+				cy.get("h1").should("contain", "React template");
+			});
+		
+			it(`(${viewport}-${orientations[i]}) - Renders about page when the /about link is clicked`, () => {
+				cy.viewport(viewport, orientations[i]) 
+				cy.visit("/");
+				cy.get(".nav-link").eq(1).click();
+				cy.get("h1").should("contain", "Welcome to the about page!");
+			});
+
+			if (viewport === "macbook-11") break; //Ensures tests only run once on this viewport. 
+		}
 	});
 });
